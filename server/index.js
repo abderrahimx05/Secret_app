ac46b67a626557f2fca9abf5148bebf098d07ca5
abderrahim <abderrahim581@hotmail.com>
2021-04-01 23:11:26 +0100

    ctreatePost

--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
 const app = express();
-app.use("/posts", postRoutes);
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes);
 const CONNECTION_URL =
   "mongodb+srv://secretmern:secretmern@cluster0.ak9fw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
